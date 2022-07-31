76a5089f116d0094eb41b5bb0803bebaca2cde65
XFTY <109122206+XFTY@users.noreply.github.com>
2022-07-31 13:27:11 +0800

    Updata

--- run.py
+++ run.py
@@ -306,7 +306,7 @@ class wordcloud_make():
 if __name__ == '__main__':
     time.sleep(1)
     print('Starting...')
-    time.sleep(3)
+    time.sleep(1)
     '''
     +=====================================+
     |        Python图形用户界面            
